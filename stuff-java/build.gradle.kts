352799b0aaeffeba118b72e07135f173a391e831
bruh <quangtrung02hn16@gmail.com>
2023-09-06 21:47:48 +0700

    Initial commit

---
+++ stuff-java/build.gradle.kts
@@ -0,0 +1,104 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("de.mannodermaus.android-junit5") version "1.9.3.0"
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.sixtyninefourtwenty.javastuff"
+    compileSdk = 34
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunnerArguments["runnerBuilder"] = "de.mannodermaus.junit5.AndroidJUnit5Builder"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        kotlinOptions.freeCompilerArgs += "-Xjvm-default=all"
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                groupId = "com.sixtyninefourtwenty"
+                artifactId = "stuff-java"
+                version = "1.0"
+
+                from(components["release"])
+
+                pom {
+                    name.set("stuff-java")
+                    description.set("Util classes for Java")
+
+                    licenses {
+                        license {
+                            name.set("The MIT License")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("unbiaseduser")
+                            name.set("Dang Quang Trung")
+                            email.set("quangtrung02hn16@gmail.com")
+                            url.set("https://github.com/unbiaseduser")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.test.ext:junit:1.1.5")
+    testImplementation("androidx.test:core-ktx:1.5.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.10.0")
+    testImplementation("org.robolectric:robolectric:4.10.3")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.3.0")
+    androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.3.0")
+    androidTestRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+}
---
+++ stuff-java/src/main/java/com/sixtyninefourtwenty/javastuff/Assets.kt
@@ -0,0 +1,25 @@
+@file:JvmName("AssetsJava")
+@file:Suppress("unused")
+
+package com.sixtyninefourtwenty.javastuff
+
+import android.content.res.AssetManager
+import android.graphics.drawable.Drawable
+import java.nio.charset.Charset
+import java.util.stream.Stream
+
+fun AssetManager.openQuietly(path: String) = open(path)
+
+@JvmOverloads
+fun AssetManager.readEntireTextFile(path: String, charset: Charset = Charsets.UTF_8) =
+    open(path).bufferedReader(charset).use { it.readText() }
+
+fun AssetManager.createDrawableFromAsset(path: String) = open(path).use { Drawable.createFromStream(it, path) }
+
+@JvmOverloads
+inline fun <R> AssetManager.readTextFileLines(path: String, charset: Charset = Charsets.UTF_8, block: (Stream<String>) -> R) =
+    open(path).bufferedReader(charset).use { block(it.lines()) }
+
+@JvmOverloads
+fun AssetManager.readFirstFileLine(path: String, charset: Charset = Charsets.UTF_8): String? =
+    open(path).bufferedReader(charset).use { it.readLine() }
