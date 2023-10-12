691223ac1b5082ff25150b208e77d9c7a14a0f11
bruh <quangtrung02hn16@gmail.com>
2023-10-12 19:45:19 +0700

    Bump dependencies

--- stuff-java/build.gradle.kts
+++ stuff-java/build.gradle.kts
@@ -83,9 +83,9 @@ afterEvaluate {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.test.ext:junit:1.1.5")
